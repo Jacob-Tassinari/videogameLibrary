e9273b595b3d9b6b126bb3be9541ff83386d61e7
Jacob Tassinari <jacobtassinari@interapts-macbook-pro-3.local>
2018-09-18 09:51:50 -0500

    finished

--- videogameLibrary/library.swift
+++ videogameLibrary/library.swift
@@ -9,8 +9,88 @@
 import Foundation
 
 class library {
-    var gameArray: [game] = [game(title: "Fortnite", genre: "shooter"), game(title: "Skyrim", genre: "rpg"), game(title: "sonic mania", genre: "platformer"), game(title: "We Happy Few", genre: "story")]
-    func addGame(){
-        
+    var gameArray: [game] = [game(title: "Fortnite"), game(title: "Skyrim"), game(title: "sonic mania"), game(title: "We Happy Few")]
+    func printAllGames(){
+        for game in gameArray {
+            print(game.title)
+        }
     }
+    func addGame(){
+        print("please input the title of the game you want to add.")
+        var newGame = readLine()
+        while newGame == nil || newGame == "" {
+            print("please input the title of the game you want to add.")
+            newGame = readLine()
+        }
+        let newGameTitle = game(title: newGame!)
+        gameArray.append(newGameTitle)
+    }
+    func checkedInGames(){
+        for game in gameArray {
+            if game.checkedIn {
+                print(game.title)
+            }
+        }
+    }
+    func checkedOutGames(){
+        for game in gameArray {
+            if game.checkedIn == false {
+                let dateFormatter = DateFormatter()
+                dateFormatter.dateFormat = "MM/dd/yyyy"
+                print("\(game.title) has a due date of \(dateFormatter.string(from: game.dueDate!))")
+            }
+        }
+    }
+    func removeGames(){
+        for index in 0..<gameArray.count {
+            print("\(index) = \(gameArray[index].title)")
+        }
+        print("please input the number id of the game you want to remove")
+        var userInput = Int(readLine()!)
+        let vaild = Array(0..<gameArray.count)
+        
+        while userInput == nil || !vaild.contains(userInput!){
+            print("please renter the number.")
+            userInput = Int(readLine()!)
+        }
+        gameArray.remove(at: userInput!)
+    }
+    func checkOu() {
+        for index in 0..<gameArray.count {
+            if gameArray[index].checkedIn == true {
+                print("\(index) = \(gameArray[index].title)")
+            }
+        }
+        print("please input the number id of the game you want to check out.")
+        var userInput = Int(readLine()!)
+        let vaild = Array(0..<gameArray.count)
+        while userInput == nil || !vaild.contains(userInput!){
+            print("please renter the number.")
+            userInput = Int(readLine()!)
+        }
+        gameArray[userInput!].checkedIn = false
+        let currentCalender = Calendar.current
+        let duedate = currentCalender.date(byAdding: .day, value: 14, to: Date())
+        gameArray[userInput!].dueDate = duedate
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "MM/dd/yyyy"
+        print("This game is due on: \(dateFormatter.string(from: duedate!))")
+    }
+    func checkIn() {
+        for index in 0..<gameArray.count {
+            if gameArray[index].checkedIn == false {
+                print("\(index) = \(gameArray[index].title)")
+            }
+        }
+        print("please input the number id of the game you want to check in.")
+        var userInput = Int(readLine()!)
+        let vaild = Array(0..<gameArray.count)
+        while userInput == nil || !vaild.contains(userInput!){
+            print("please renter the number.")
+            userInput = Int(readLine()!)
+        }
+        gameArray[userInput!].checkedIn = true
+        gameArray[userInput!].dueDate = nil
+    }
+    
 }
--- videogameLibrary/meny.swift
+++ videogameLibrary/meny.swift
@@ -7,3 +7,70 @@
 //
 
 import Foundation
+
+var  shouldQuit = false
+var userInnput = 0
+class menu {
+    let lib = library()
+    func go() {
+        while !shouldQuit{
+            printMenu()
+            handleinput(getInput())
+        }
+    }
+    func printMenu(){
+        print("add game(1) - remove games(2) - list all games(3) - list all games in currently(4) - list all of the games out(5) - check out(6)   - check in(7) - quit program(8) - reprint menu(9)")
+    }
+
+    func quit(){
+        shouldQuit = true
+        print("Thanks for stoping by.")
+    }
+    func getInput() -> String {
+        var userInput = readLine()
+        while userInput == nil || userInput == "" {
+            print("Please give a valid input")
+            userInput = readLine()
+        }
+        return userInput!
+    }
+    func  handleinput(_ input: String) {
+        switch getInput() {
+        case "1":
+            print("add game(1)")
+            lib.addGame()
+        case "2":
+            print("remove games(2)")
+            lib.removeGames()
+        case "3":
+            print("list all games(3)")
+            lib.printAllGames()
+        case "4":
+            print("list all games in currently(4)")
+            lib.checkedInGames()
+        case "5":
+            print("list all of the games out(5)")
+            lib.checkedOutGames()
+        case "6":
+            print("check out(6)")
+            lib.checkOu()
+        case "7":
+            print("check in(7)")
+            lib.checkIn()
+        case "8":
+            print("quit program(8)")
+            quit()
+        case "9":
+            printMenu()
+        default:
+            break
+        }
+    }
+    func vaildateInput(_ Input:String) -> Bool {
+        let arrayCase = Array(1...9)
+        guard let number  = Int(Input) else {
+            return false
+        }
+        return arrayCase.contains(number)
+    }
+}
