6ec8135203dc8bb738435dea78c6ab9ab7810c8b
Jacob Tassinari <jacobtassinari@Interapts-MacBook-Pro-3.local>
2018-09-18 09:50:42 -0500

    finshed have to type twice

---
+++ videogameLibrary/library.swift
@@ -0,0 +1,16 @@
+//
+//  library.swift
+//  videogameLibrary
+//
+//  Created by Jacob Tassinari on 9/13/18.
+//  Copyright © 2018 Jacob Tassinari. All rights reserved.
+//
+
+import Foundation
+
+class library {
+    var gameArray: [game] = [game(title: "Fortnite", genre: "shooter"), game(title: "Skyrim", genre: "rpg"), game(title: "sonic mania", genre: "platformer"), game(title: "We Happy Few", genre: "story")]
+    func addGame(){
+        
+    }
+}
---
+++ videogameLibrary/meny.swift
@@ -0,0 +1,9 @@
+//
+//  meny.swift
+//  videogameLibrary
+//
+//  Created by Jacob Tassinari on 9/11/18.
+//  Copyright © 2018 Jacob Tassinari. All rights reserved.
+//
+
+import Foundation
